d0d1784818fd7516b8f59f19fc8f846fb6ed486a
Kamil Gabryjelski <kamil.gabryjelski@gmail.com>
2019-10-09 17:36:20 +0200

    Add initial Hugo project (#64)

---
+++ landing-pages/webpack.prod.js
@@ -0,0 +1,51 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+const merge = require("webpack-merge");
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+const common = require("./webpack.common.js");
+
+module.exports = merge(common, {
+  mode: "production",
+
+  output: {
+    filename: "[name].[hash:5].js",
+    chunkFilename: "[id].[hash:5].css"
+  },
+
+  optimization: {
+    minimizer: [
+      new UglifyJsPlugin({
+        cache: true,
+        parallel: true,
+        sourceMap: true
+      }),
+
+      new MiniCssExtractPlugin({
+        filename: "[name].[hash:5].css",
+        chunkFilename: "[id].[hash:5].css"
+      }),
+
+      new OptimizeCSSAssetsPlugin({})
+    ]
+  }
+});
