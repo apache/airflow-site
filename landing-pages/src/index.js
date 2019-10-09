2d466fbe32b36ec9154759d286e3953da9c67d82
Kamil Breguła <kamil.bregula@polidea.com>
2019-10-09 20:37:23 +0200

    Add Travis CI + Docker integration

--- landing-pages/src/index.js
+++ landing-pages/src/index.js
@@ -16,7 +16,3 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-
-console.log("🦊 Hello! Edit me in src/index.js");
-export const foo = () => console.log("foo");
-foo()
