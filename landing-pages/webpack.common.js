c4f7a81b8c88b9cbfaa06a022f96847f0dd9a49a
Kamil Gabryjelski <kamil.gabryjelski@gmail.com>
2019-10-09 17:36:20 +0200

    Add initial Hugo project (#64)

---
+++ landing-pages/webpack.common.js
@@ -0,0 +1,78 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+const webpack = require("webpack");
+const path = require("path");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const AssetsPlugin = require("assets-webpack-plugin");
+
+module.exports = {
+  entry: {
+    main: path.join(__dirname, "src", "index.js")
+  },
+
+  output: {
+    path: path.join(__dirname, "dist")
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.((png)|(eot)|(woff)|(woff2)|(ttf)|(svg)|(gif))(\?v=\d+\.\d+\.\d+)?$/,
+        loader: "file-loader?name=/[hash].[ext]"
+      },
+
+      {test: /\.json$/, loader: "json-loader"},
+
+      {
+        loader: "babel-loader",
+        test: /\.js?$/,
+        exclude: /node_modules/,
+        query: {cacheDirectory: true}
+      },
+
+      {
+        test: /\.(sa|sc|c)ss$/,
+        exclude: /node_modules/,
+        use: ["style-loader", MiniCssExtractPlugin.loader, "css-loader", "postcss-loader", "sass-loader"]
+      }
+    ]
+  },
+
+  plugins: [
+    new webpack.ProvidePlugin({
+      fetch: "imports-loader?this=>global!exports-loader?global.fetch!whatwg-fetch"
+    }),
+
+    new AssetsPlugin({
+      filename: "webpack.json",
+      path: path.join(process.cwd(), "site/data"),
+      prettyPrint: true
+    }),
+
+    new CopyWebpackPlugin([
+      {
+        from: "./src/fonts/",
+        to: "fonts/",
+        flatten: true
+      }
+    ])
+  ]
+};
