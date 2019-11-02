8aaaf4846a28805bacb4045366f2a30b04ffce54
Kamil Breguła <kamil.bregula@polidea.com>
2019-11-02 19:07:45 +0100

    Lint all JS files

--- landing-pages/create-index.js
+++ landing-pages/create-index.js
@@ -17,94 +17,96 @@
  * under the License.
  */
 
-const fs = require('fs').promises;
-const path = require('path');
-const { promisify } = require('util');
-const frontMatterParser = require('parser-front-matter');
+/* eslint-disable no-console */
+
+const fs = require("fs").promises;
+const path = require("path");
+const {promisify} = require("util");
+const frontMatterParser = require("parser-front-matter");
 const parse = promisify(frontMatterParser.parse.bind(frontMatterParser));
-const lunrjs = require('lunr');
+const lunrjs = require("lunr");
 
 const contentDirectory = `${__dirname}/site/content`;
 const outputtDirectory = `${__dirname}/site/static/indexes`;
 
 
 async function isDirectoryExists(dirPath) {
-    try {
-        if ((await fs.stat(dirPath)).isDirectory()) {
-            return true;
-        }
-    }catch (err) {
-        return false
+  try {
+    if ((await fs.stat(dirPath)).isDirectory()) {
+      return true;
     }
-    return false
+  } catch (err) {
+    return false;
+  }
+  return false;
 }
 
 async function loadPostsWithFrontMatter(postsDirectoryPath) {
-    const fileNames = await fs.readdir(postsDirectoryPath);
-    const posts = await Promise.all(
-        fileNames.map(async fileName => {
-            const fileContent = await fs.readFile(
-                `${postsDirectoryPath}/${fileName}`,
-                'utf8'
-            );
-            const {content, data} = await parse(fileContent);
-            return {
-                content: content.slice(0, 3000),
-                url: path.parse(fileName).name,
-                ...data
-            };
-        })
-    );
-    return posts;
+  const fileNames = await fs.readdir(postsDirectoryPath);
+  const posts = await Promise.all(
+    fileNames.map(async(fileName) => {
+      const fileContent = await fs.readFile(
+        `${postsDirectoryPath}/${fileName}`,
+        "utf8"
+      );
+      const {content, data} = await parse(fileContent);
+      return {
+        content: content.slice(0, 3000),
+        url: path.parse(fileName).name,
+        ...data
+      };
+    })
+  );
+  return posts;
 }
 
 function makeIndex(posts) {
-    return lunrjs(function() {
-        this.ref('title');
-        this.field('title');
-        this.field("description");
-        this.field("author");
-        this.field('content');
-        this.field('tags');
-        this.field('url');
-        posts.forEach(p => {
-            this.add(p);
-        });
+  return lunrjs(function() {
+    this.ref("title");
+    this.field("title");
+    this.field("description");
+    this.field("author");
+    this.field("content");
+    this.field("tags");
+    this.field("url");
+    posts.forEach((p) => {
+      this.add(p);
     });
+  });
 }
 
 async function writeJson(filePath, index) {
-    await fs.writeFile(filePath, JSON.stringify(index));
-    console.log(`Saved ${path.parse(filePath).base} file.`)
+  await fs.writeFile(filePath, JSON.stringify(index));
+  console.log(`Saved ${path.parse(filePath).base} file.`);
 }
 
 async function processLanguage(language) {
-    console.log(`Proccessing "${language}" language.`)
-    const currentDirectory = `${contentDirectory}/${language}/blog`;
-    if (!await isDirectoryExists(currentDirectory)) {
-        console.log("No blog posts. Skipping.")
-        return
-    }
-    const posts = await loadPostsWithFrontMatter(currentDirectory);
-    const index = makeIndex(posts);
-    const currentOutputDirectory = `${outputtDirectory}/${language}`;
-    if (!await isDirectoryExists(currentOutputDirectory)) {
-        await fs.mkdir(currentOutputDirectory)
-    }
-    await writeJson(`${currentOutputDirectory}/blog-index.json`, index);
-    await writeJson(`${currentOutputDirectory}/blog-posts.json`, posts)
+  console.log(`Proccessing "${language}" language.`);
+  const currentDirectory = `${contentDirectory}/${language}/blog`;
+  if (!await isDirectoryExists(currentDirectory)) {
+    console.log("No blog posts. Skipping.");
+    return;
+  }
+  const posts = await loadPostsWithFrontMatter(currentDirectory);
+  const index = makeIndex(posts);
+  const currentOutputDirectory = `${outputtDirectory}/${language}`;
+  if (!await isDirectoryExists(currentOutputDirectory)) {
+    await fs.mkdir(currentOutputDirectory);
+  }
+  await writeJson(`${currentOutputDirectory}/blog-index.json`, index);
+  await writeJson(`${currentOutputDirectory}/blog-posts.json`, posts);
 }
 
 async function run() {
-    const directoryNames = await fs.readdir(contentDirectory);
-    for (const directoryName of directoryNames){
-        await processLanguage(directoryName)
-    }
+  const directoryNames = await fs.readdir(contentDirectory);
+  for (const directoryName of directoryNames) {
+    await processLanguage(directoryName);
+  }
 }
 
 run()
-    .then(() => process.exit(0))
-    .catch(error => {
-        console.error(error.stack);
-        process.exit(1);
-    });
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error.stack);
+    process.exit(1);
+  });
