70953203c7de295c5f5310ec21c2e068584e6f8b
Jarek Potiuk <jarek.potiuk@polidea.com>
2020-08-06 09:52:15 +0200

    Remove renundant empty line (#277)
    Triggering build

--- site.sh
+++ site.sh
@@ -16,7 +16,6 @@
 # KIND, either express or implied.  See the License for the
 # specific language governing permissions and limitations
 # under the License.
-
 set -euox pipefail
 
 WORKING_DIR="$(pwd)"
