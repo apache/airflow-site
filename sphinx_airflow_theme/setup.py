bc4c650f39a30ff716c8afa218a5f5dd8f4e5c40
Ash Berlin-Taylor <ash_github@firemirror.com>
2021-06-02 16:00:19 +0100

    Set __version__ of sphinx_airflow_theme module correctly (#427)
    Using this feature of setuptools 46.4.0+ we only need to specify it
    once, and don't have to handle parsing it either

--- sphinx_airflow_theme/setup.py
+++ sphinx_airflow_theme/setup.py
@@ -38,7 +38,6 @@ with open('README.md', encoding='utf-8') as file:
 
 setup(
     name='sphinx_airflow_theme',
-    version='0.0.4',
     url='https://github.com/apache/airflow-site/tree/aip-11',
     license='Apache License 2.0',
     author='Apache Software Foundation',
