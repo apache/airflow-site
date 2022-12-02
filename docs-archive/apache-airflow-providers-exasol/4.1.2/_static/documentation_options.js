e0cbe88840fe32c9c9f86cec8a81cb529b0c6ee6
Jarek Potiuk <jarek@potiuk.com>
2022-12-02 12:34:16 +0100

    Fix stderr redirection (#693)
    * Add documentation for packages - 2022-12-01
    
    * Fix stderr creeping into version when preparing the theme

---
+++ docs-archive/apache-airflow-providers-exasol/4.1.2/_static/documentation_options.js
@@ -0,0 +1,17 @@
+var DOCUMENTATION_OPTIONS = {
+    URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
+<<<<<<< HEAD:docs-archive/apache-airflow-providers/_static/documentation_options.js
+    VERSION: 'master',
+    LANGUAGE: 'None',
+=======
+    VERSION: '4.1.2',
+    LANGUAGE: 'en',
+>>>>>>> fea1830d03 (Fix stderr redirection (#693)):docs-archive/apache-airflow-providers-exasol/4.1.2/_static/documentation_options.js
+    COLLAPSE_INDEX: false,
+    BUILDER: 'html',
+    FILE_SUFFIX: '.html',
+    LINK_SUFFIX: '.html',
+    HAS_SOURCE: true,
+    SOURCELINK_SUFFIX: '.txt',
+    NAVIGATION_WITH_KEYS: false
+};
