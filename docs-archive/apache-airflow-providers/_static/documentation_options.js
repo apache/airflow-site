220dbbe1195b3d4ea3781886f2fe3dc2d965a9e0
Jarek Potiuk <jarek@potiuk.com>
2022-12-02 12:47:27 +0100

    Fix stderr redirection fix (#694)
    Fixing accidentally merged docs change.
    
    * Revert "Fix stderr redirection (#693)"
    
    This reverts commit fea1830d03c0b7b7c45f34ae447338b4073f3152.
    
    * Fix stderr creeping into version when preparing the theme

---
+++ docs-archive/apache-airflow-providers/_static/documentation_options.js
@@ -0,0 +1,21 @@
+var DOCUMENTATION_OPTIONS = {
+    URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
+<<<<<<< HEAD:docs-archive/apache-airflow-providers-exasol/4.1.2/_static/documentation_options.js
+<<<<<<< HEAD:docs-archive/apache-airflow-providers/_static/documentation_options.js
+    VERSION: 'master',
+    LANGUAGE: 'None',
+=======
+    VERSION: '4.1.2',
+=======
+    VERSION: 'devel',
+>>>>>>> d2712865f3 (Fix stderr redirection fix (#694)):docs-archive/apache-airflow-providers/_static/documentation_options.js
+    LANGUAGE: 'en',
+>>>>>>> fea1830d03 (Fix stderr redirection (#693)):docs-archive/apache-airflow-providers-exasol/4.1.2/_static/documentation_options.js
+    COLLAPSE_INDEX: false,
+    BUILDER: 'html',
+    FILE_SUFFIX: '.html',
+    LINK_SUFFIX: '.html',
+    HAS_SOURCE: true,
+    SOURCELINK_SUFFIX: '.txt',
+    NAVIGATION_WITH_KEYS: false
+};
