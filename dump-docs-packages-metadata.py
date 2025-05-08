18fcf996961f2902570c404bed689164133e78c3
Pavan Kumar <gopidesupavan@gmail.com>
2025-05-08 23:01:58 +0100

    Update s3 bucket

--- dump-docs-packages-metadata.py
+++ dump-docs-packages-metadata.py
@@ -3,7 +3,7 @@ import sys
 
 import requests
 
-resp = requests.get("https://staging-docs-airflow-apache-org.s3.us-east-2.amazonaws.com/manifest/packages-metadata.json")
+resp = requests.get("https://live-docs-airflow-apache-org.s3.us-east-2.amazonaws.com/manifest/packages-metadata.json")
 if resp.status_code != 200:
     raise RuntimeError(f"Failed to fetch metadata: {resp.status_code} - {resp.text}")
 
