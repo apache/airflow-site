a2b8f0fb7a89b5154b140474db4bd7d1b2964f0b
Jarek Potiuk <jarek@potiuk.com>
2025-10-16 09:49:44 +0200

    Improve the script to refresh images

--- .github/scripts/refresh_pmc_committer_images.py
+++ .github/scripts/refresh_pmc_committer_images.py
@@ -19,21 +19,30 @@
 # /// script
 # requires-python = ">=3.9"
 # dependencies = [
+#   "rich>=13.3.4",
 #   "requests>=2.32.2",
 # ]
 # ///
 
 import os
+from pathlib import Path
+
 import requests
 import json
-def refresh_committer_pmc_images(file_path):
+from rich.console import Console
 
+console = Console(width=400, color_system="standard")
+
+AIRFLOW_SOURCES_ROOT = Path(__file__).parents[2]
+
+def refresh_committer_pmc_images(file_path: Path):
     with open(file_path) as f:
         pmc_committer_data = json.load(f)
     for data in pmc_committer_data:
         github_url = data.get("github")
         username = github_url.split("/")[-1]
         image_url = github_url+'.png'
+        console.print(f"Downloading image for: [magenta]{data.get("name")}[/] from {image_url}")
         response = requests.get(image_url)
         if response.status_code == 200:
             with open(f'landing-pages/site/static/external/profiles/{username}.png', 'wb') as f:
@@ -43,8 +52,8 @@ def refresh_committer_pmc_images(file_path):
 
 
 if __name__ == "__main__":
-    pmc_committer_files = os.environ.get("PMC_COMMITTERS_FILES")
+    pmc_committer_files = os.environ.get("PMC_COMMITTERS_FILES",
+                                         "landing-pages/site/data/committers.json,landing-pages/site/data/pmc.json")
     if pmc_committer_files:
-        file_paths = pmc_committer_files.split(",")
-        for file_path in file_paths:
-            refresh_committer_pmc_images(file_path)
+        for file_path in pmc_committer_files.split(","):
+            refresh_committer_pmc_images(AIRFLOW_SOURCES_ROOT / file_path)
