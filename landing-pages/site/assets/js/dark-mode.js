d1749cf0fd6f1f22ecf0c54d2a4beec73f41fc8b
Shahar Epstein <60007259+shahar1@users.noreply.github.com>
2026-01-03 20:12:36 +0200

    Fix dark mode button on production #2 (#1372)

--- landing-pages/site/assets/js/dark-mode.js
+++ landing-pages/site/assets/js/dark-mode.js
@@ -103,23 +103,15 @@
     }
   })
 
-  const init = () => {
-    showActiveTheme(getPreferredTheme())
+  showActiveTheme(getPreferredTheme())
 
-    document.querySelectorAll('[data-bs-theme-value]')
-      .forEach(toggle => {
-        toggle.addEventListener('click', () => {
-          const theme = toggle.getAttribute('data-bs-theme-value')
-          setStoredTheme(theme)
-          setTheme(theme)
-          showActiveTheme(theme, true)
-        })
+  document.querySelectorAll('[data-bs-theme-value]')
+    .forEach(toggle => {
+      toggle.addEventListener('click', () => {
+        const theme = toggle.getAttribute('data-bs-theme-value')
+        setStoredTheme(theme)
+        setTheme(theme)
+        showActiveTheme(theme, true)
       })
-  }
-
-  if (document.readyState === 'loading') {
-    window.addEventListener('DOMContentLoaded', init)
-  } else {
-    init()
-  }
+    })
 })()
